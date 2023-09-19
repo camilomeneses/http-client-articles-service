e63dcb125613cd2d1a2a8d0ef3bb270279630814
Camilo Meneses <kamilomeneses@gmail.com>
2023-09-19 17:29:15 -0500

    add initial files

---
+++ src/test/java/dev/camilo/articlesservice/ArticlesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.camilo.articlesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticlesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
